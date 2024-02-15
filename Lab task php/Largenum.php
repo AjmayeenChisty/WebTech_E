8aee8c2a3ccd76efefe8192901dd21447ab98edc
Ajmayeen Chisty <158998205+AjmayeenChisty@users.noreply.github.com>
2024-02-15 13:52:52 +0600

    Add files via upload

---
+++ Lab task php/Largenum.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 132;
+$b = 218;
+$c = 208;
+
+
+if($a > $b && $a > $c){
+    echo "a is the largest number";
+}else if($b > $a && $b > $c){
+    echo "b is the largest number";
+}else{
+    echo "c is the largest number";
+}
+
+?>
